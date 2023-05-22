1693f6f731b49ffa1640bf17457b74da564b4210
talha <talha.jamil@devsinc.com>
2023-05-22 16:00:37 +0500

    Make django project and create app for posts

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images")
+    category = models.CharField(max_length=128, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
